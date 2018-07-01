eb6c57bb4a575063739318b478dd9d59188cd9d6
seabassgonzalez <seabassgonzalez@gmail.com>
2018-07-01 15:10:01 -0700

    Change Layout component to class based instantiation pattern

--- components/layout.js
+++ components/layout.js
@@ -1,30 +1,39 @@
 // import Head from next
+// import React and Component from react
 // import Navbar from navbar
 
-// const Layout taking props fat arrow function
-	// div
-		// Head section
-			// title
-			// link set to bootstrap stylesheet
-		// Navbar component
-		// reference to props children
+// class Layout extending Component
+	// render
+		// return
+			// div
+				// Head section
+					// title
+					// link set to bootstrap stylesheet
+				// Navbar component
+				// div className set to bootstrap flex container and justified to center
+					// reference to this.props.children
 
 // export default Layout
 
 import Head from 'next/head';
+import React, { Component } from 'react';
 import Navbar from './navbar';
 
-const Layout = (props) => (
-	<div>
-		<Head>
-			<title>Crypto's Right</title>
-			<link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css"/>
-		</Head>
-		<Navbar />
-		<div className="d-flex justify-content-center container">
-			{props.children}
-		</div>
-	</div>
-);
+class Layout extends Component{
+	render(){
+		return(
+			<div>
+				<Head>
+					<title>Crypto's Right</title>
+					<link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css"/>
+				</Head>
+				<Navbar />
+				<div className="d-flex justify-content-center container">
+					{this.props.children}
+				</div>
+			</div>
+		)
+	}
+};
 
 export default Layout;
