25a7cbaa77da3ae52a39c3991dd70031a1f95efb
seabassgonzalez <seabassgonzalez@gmail.com>
2018-07-05 16:27:37 -0700

    Implement practice fetch api call to tv show api within getInitialProps, class based instantiation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-// import Layout component
 // import React and Component from react
+// import Fetch from isomorphic-unfetch
+// import Layout component
 
 // class Index extending Component
 	// render
@@ -12,17 +13,35 @@
 
 // export default Index
 
-import Layout from '../components/layout';
 import React, { Component } from 'react';
+import Layout from '../components/layout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
 class Index extends Component {
+	static async getInitialProps(){
+		const res = await fetch('https://api.tvmaze.com/search/shows?q=seinfeld');
+		const data = await res.json();
+		console.log('show data fetched, count', data.length);
+		return{
+			shows:data
+		}	
+	}
 	render(){
 		return (
 			<Layout>
 				<div>
-					<div>
-						<h1>Welcome to Crypto's Right</h1>
-					</div>
+					<h1>Welcome to Crypto's Right</h1>
+					<h1>Temporary Seinfeld TV Show Get Initial Props Practice</h1>
+					<ul>
+						{this.props.shows.map(({show}) => (
+							<li key={show.id}>
+								<Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}> 
+									<a>{show.name}</a>
+								</Link>
+							</li>
+						))}
+					</ul>
 				</div>
 			</Layout>
 		)
